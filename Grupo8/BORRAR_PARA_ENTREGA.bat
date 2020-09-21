77e77f793aa58e8b23a827340f646e4e2b3f53ad
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-09-21 18:01:34 -0300

    Subi los archivos para empezar =)

---
+++ Grupo8/BORRAR_PARA_ENTREGA.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:do_while
+echo Creando el archivo lex.yy.c.
+c:\GnuWin32\bin\flex Lexico.l
+pause
+echo Creando el ejecutable ...
+c:\MinGW\bin\gcc.exe lex.yy.c -o Primera.exe
+pause
+cls
+echo Ejecutando el Analizador Lexicografico
+echo RESULTADOS:
+echo -----------------------------------------------------------------
+Primera.exe prueba.txt
+echo.
+echo -----------------------------------------------------------------
+pause
+del lex.yy.c
+del ejecutable.exe
+cls
+set /p seguir=Desea seguir con el programa? [s/n]
+if "%seguir%"=="s" goto do_while

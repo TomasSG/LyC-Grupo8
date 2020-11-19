baef561f6f53d73f0554f3941ddcc5a5ac33425c
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-11-19 13:30:35 -0300

    Nuevo formato a la ts

--- Grupo8/Final.asm
+++ Grupo8/Final.asm
@@ -9,7 +9,7 @@ include macros2.asm
 @aux                               	dd	?
 @cant                              	dd	?
 _0123456789                        	db	"0123456789"                       , '$', 10 dup (?)
-_ABCDEFGHIJKLMN__OPQRSTUVWYXZ      	db	"ABCDEFGHIJKLMNÑOPQRSTUVWYXZ"     , '$', 28 dup (?)
+_ABCDEFGHIJKLMNOPQRSTUVWYXZ        	db	"ABCDEFGHIJKLMNOPQRSTUVWYXZ"       , '$', 26 dup (?)
 _Correcto_                         	db	"Correcto!"                        , '$', 9 dup (?)
 _Hola_Mundo_                       	db	"Hola Mundo!"                      , '$', 11 dup (?)
 _Ingrese_el_numero__               	db	"Ingrese el numero: "              , '$', 19 dup (?)
@@ -23,7 +23,7 @@ MOV ES, EAX
 
 displayString _Hola_Mundo_
 displayString _abcdefghijklmnopqrstuvwyxz
-displayString _ABCDEFGHIJKLMN__OPQRSTUVWYXZ
+displayString _ABCDEFGHIJKLMNOPQRSTUVWYXZ
 displayString _0123456789
 displayString _Ingrese_el_numero__
 displayString _Correcto_

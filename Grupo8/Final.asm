9052b6fd6d95c87bbede1c4d98abd6ef98828e6a
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-11-22 20:13:17 -0300

    Agregado un caso mas para contar

--- Grupo8/Final.asm
+++ Grupo8/Final.asm
@@ -18,6 +18,7 @@ _2                                 	dd	2.00
 _3                                 	dd	3.00
 _4                                 	dd	4.00
 _8                                 	dd	8.00
+__5                                	dd	.5
 _resultado_esperado__0             	db	"Resultado esperado: 0"            , '$', 21 dup (?)
 _resultado_esperado__1             	db	"Resultado esperado: 1"            , '$', 21 dup (?)
 _resultado_esperado__2             	db	"Resultado esperado: 2"            , '$', 21 dup (?)
@@ -412,6 +413,87 @@ FLD _1
 FADD
 FSTP @cant
 _ET_189:
+FLD @cant
+FSTP a
+DisplayString _resultado_esperado__2
+newline 1
+DisplayFloat a, 2
+newline 1
+FLD _2
+FSTP b
+FLD _4
+FSTP c
+FLD b
+FLD c
+FDIV
+FSTP @aux
+FLD _1_2
+FLD _0
+FSTP @cant
+FLD @aux
+FXCH
+FCOM
+FSTSW AX
+SAHF
+FFREE
+JNE _ET_208
+FLD @cant
+FLD _1
+FADD
+FSTP @cant
+_ET_208:
+FLD __5
+FLD @aux
+FXCH
+FCOM
+FSTSW AX
+SAHF
+FFREE
+JNE _ET_214
+FLD @cant
+FLD _1
+FADD
+FSTP @cant
+_ET_214:
+FLD _8
+FLD @aux
+FXCH
+FCOM
+FSTSW AX
+SAHF
+FFREE
+JNE _ET_220
+FLD @cant
+FLD _1
+FADD
+FSTP @cant
+_ET_220:
+FLD __5
+FLD @aux
+FXCH
+FCOM
+FSTSW AX
+SAHF
+FFREE
+JNE _ET_226
+FLD @cant
+FLD _1
+FADD
+FSTP @cant
+_ET_226:
+FLD _8
+FLD @aux
+FXCH
+FCOM
+FSTSW AX
+SAHF
+FFREE
+JNE _ET_232
+FLD @cant
+FLD _1
+FADD
+FSTP @cant
+_ET_232:
 FLD @cant
 FSTP a
 DisplayString _resultado_esperado__2

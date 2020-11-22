93e03d9dd0c8b23032449445158622d161897d79
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-11-22 19:47:22 -0300

    Test de put

--- Grupo8/Final.asm
+++ Grupo8/Final.asm
@@ -8,16 +8,11 @@ include macros2.asm
 .DATA
 @aux                               	dd	?
 @cant                              	dd	?
-_a___b                             	db	"a < b"                            , '$', 5 dup (?)
-_a__b                              	db	"a >b"                             , '$', 4 dup (?)
-_c____b                            	db	"c <= b"                           , '$', 6 dup (?)
-_c___b                             	db	"c > b"                            , '$', 5 dup (?)
-_ingrese_un_valor_a                	db	"Ingrese un valor a"               , '$', 18 dup (?)
-_ingrese_un_valor_b                	db	"Ingrese un valor b"               , '$', 18 dup (?)
-_ingrese_un_valor_c                	db	"Ingrese un valor c"               , '$', 18 dup (?)
-a                                  	dd	?
-b                                  	dd	?
-c                                  	dd	?
+_________                          	db	"@_-.!'?*"                         , '$', 8 dup (?)
+_c                                 	db	"c"                                , '$', 1 dup (?)
+_string_con_123                    	db	"String con 123"                   , '$', 14 dup (?)
+_string_dif_ic_il_12_              	db	"String dif ic il 12!"             , '$', 20 dup (?)
+_string_facil                      	db	"String facil"                     , '$', 12 dup (?)
 
 .CODE
 START:
@@ -25,67 +20,16 @@ MOV EAX, @DATA
 MOV DS, EAX
 MOV ES, EAX
 
-DisplayString _ingrese_un_valor_a
+DisplayString _c
 newline 1
-GetFloat a
-DisplayString _ingrese_un_valor_b
+DisplayString _string_facil
 newline 1
-GetFloat b
-FLD a
-FLD b
-FXCH
-FCOM
-FSTSW AX
-SAHF
-FFREE
-JNE _ET_26
-_ET_8:
-DisplayString _ingrese_un_valor_c
+DisplayString _string_con_123
 newline 1
-GetFloat c
-FLD c
-FLD b
-FXCH
-FCOM
-FSTSW AX
-SAHF
-FFREE
-JBE _ET_17
-_ET_15:
-DisplayString _c___b
+DisplayString _string_dif_ic_il_12_
 newline 1
-_ET_17:
-FLD c
-FLD b
-FXCH
-FCOM
-FSTSW AX
-SAHF
-FFREE
-JNBE _ET_24
-_ET_22:
-DisplayString _c____b
+DisplayString _________
 newline 1
-_ET_24:
-JMP _ET_37
-_ET_26:
-FLD a
-FLD b
-FXCH
-FCOM
-FSTSW AX
-SAHF
-FFREE
-JNB _ET_34
-_ET_31:
-DisplayString _a___b
-newline 1
-JMP _ET_36
-_ET_34:
-DisplayString _a__b
-newline 1
-_ET_36:
-_ET_37:
 
 MOV EAX, 4C00H
 INT 21h
